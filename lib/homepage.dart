08068df3ead8312670b6b797ca029972f5ef2ce7
Octowiaz <octavius0044@gmail.com>
2026-03-26 11:35:22 +0800

    Week 7

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/barrier.dart';
 import 'package:flutter_demo/player.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,28 +17,48 @@ class _HomePageState extends State<HomePage> {
   double time = 0;
   double gravity = -2.45; //gravity strength
   double velocity = 3; //jump strength
+  double circleWidth = 0.1;
+  double circleHeight = 0.1;
 
   bool gameHasStarted = false;
 
+  static List<double> barrierX = [2, 2 + 1.5];
+  static double barrierWidth = 0.5;
+  List<List<double>> barrierHeigth = [
+    [0.6, 0.4],
+    [0.4, 0.6],
+  ];
+
   void startGame() {
     gameHasStarted = true;
-    Timer.periodic(Duration(milliseconds: 50), (timer) {
+    Timer.periodic(Duration(milliseconds: 100), (timer) {
 
       height = gravity * time * time + velocity * time;
 
-      
       setState(() {
         circleY = initialPos - height;
       });
 
-      print(circleY);
+    if (circleIsDead()) {
+      timer.cancel();
+      _showDialog();
+    }
 
-      if (circleY > 1) {
-        timer.cancel();
-      }
+      moveMap();
 
       time += 0.1;
     });
+  }
+
+  void moveMap() {
+    for (int i = 0; i < barrierX.length; i++) {
+      setState(() {
+        barrierX[i] -= 0.005;
+      });
+      if (barrierX[i] < -1.5) {
+        barrierX[i] += 3;
+      }
+    }
   }
 
   void jump() {
@@ -47,6 +68,65 @@ class _HomePageState extends State<HomePage> {
     }); 
   }
 
+  void resetGame() {
+    Navigator.pop(context);
+    setState(() {
+      circleY = 0;
+      gameHasStarted = false;
+      time = 0;
+      initialPos = circleY;
+    });
+  }
+
+  void _showDialog() {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        backgroundColor: Colors.brown,
+        title: Center(
+          child: Text(
+            "G A M E  O V E R",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: resetGame,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(5),
+              child: Container(
+                padding: EdgeInsets.all(7),
+                color: Colors.white,
+                child: Text(
+                  "PLAY AGAIN",
+                  style: TextStyle(color: Colors.brown),
+                ),
+              ),
+            ),
+          )
+        ],
+      );
+    });
+}
+
+  bool circleIsDead() {
+    if (circleY < -1 || circleY > 1) {
+      return true;
+    }
+
+    for (int i = 0; i < barrierX.length; i++) {
+      if (barrierX[i] <= circleWidth &&
+          barrierX[i] +  barrierWidth >= -circleWidth &&
+          (circleY <= -1 + barrierHeigth[i] [0] ||
+              circleY + circleHeight >= 1 - barrierHeigth[i][1])) {
+        return true;
+      }
+    }
+
+    return false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -64,7 +144,38 @@ class _HomePageState extends State<HomePage> {
                     children: [
                       Player(
                         circleY: circleY,
+                        circleWidth: circleWidth,
+                        circleHeight: circleHeight,
                       ),
+
+                      MyBarrier(
+                        barrierX: barrierX[0],
+                        barrierWidth: barrierWidth,
+                        barrierHeight: barrierHeigth[0][0],
+                        isThisBottomBarrier: false,
+                      ),
+
+                      MyBarrier(
+                        barrierX: barrierX[0],
+                        barrierWidth: barrierWidth,
+                        barrierHeight: barrierHeigth[0][1],
+                        isThisBottomBarrier: true,
+                      ),
+
+                      MyBarrier(
+                        barrierX: barrierX[1],
+                        barrierWidth: barrierWidth,
+                        barrierHeight: barrierHeigth[1][0],
+                        isThisBottomBarrier: false,
+                      ),
+
+                      MyBarrier(
+                        barrierX: barrierX[1],
+                        barrierWidth: barrierWidth,
+                        barrierHeight: barrierHeigth[1][1],
+                        isThisBottomBarrier: true,
+                      ),
+
                       Container(
                           alignment: Alignment(0, -0.5),
                           child: Text(
--- lib/player.dart
+++ lib/player.dart
@@ -2,16 +2,20 @@ import 'package:flutter/material.dart';
 
 class Player extends StatelessWidget {
   final circleY;
+  final double circleWidth;
+  final double circleHeight;
 
-  Player({this.circleY});
+  Player({this.circleY, required this.circleWidth, required this.circleHeight});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        alignment: Alignment(0, circleY),
+        alignment: Alignment(0, (2 * circleY + circleHeight) / (2 - circleHeight)),
         child: Image.asset(
           'lib/images/player.png',
-          width: 50,
+          width: MediaQuery.of(context).size.height * circleWidth / 2,
+          height: MediaQuery.of(context).size.height * 3 / 4 * circleHeight / 2,
+          fit: BoxFit.fill,
         ));
   }
 }
